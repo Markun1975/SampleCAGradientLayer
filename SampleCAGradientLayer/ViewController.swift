e0de57df641a45caa981f6a864df85e04e9a1cfc
= <=>
2021-11-14 21:02:28 +0900

    CAGradientLayer実装

--- SampleCAGradientLayer/ViewController.swift
+++ SampleCAGradientLayer/ViewController.swift
@@ -2,18 +2,25 @@
 //  ViewController.swift
 //  SampleCAGradientLayer
 //
-//  Created by Masaki on R 3/11/14.
+//  Created by Masaki on R 3/11/08.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var gradationView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let gradientLayer = CAGradientLayer()
+        let color1 = UIColor.red
+        let color2 = UIColor.blue
+        gradientLayer.colors = [color1.cgColor,color2.cgColor] //ここでグラデーションを構成する複数の色を設定する
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
+        gradientLayer.frame = gradationView.bounds //gradationViewにgradientLayerを当てはめる
+        gradationView.layer.insertSublayer(gradientLayer, at: 0)
     }
-
-
 }
-
